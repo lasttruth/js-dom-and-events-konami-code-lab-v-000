cd683ede9e575fe64ed6b96522e49a30fab4a531
Robin Doirin <robindoirin@gmail.com>
2019-07-11 02:42:20 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -4,22 +4,17 @@ function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
 
-  document.body.addEventListener('keydown', function onKeyDownHandler(e) {
-	  const key = parseInt(e.detail || e.which);
+  document.body.addEventListener('keydown', function(e){
+   const key = parseInt(e.detail || e.which);
 
-    let index = 0;
-
- document.body.addEventListener('keydown', function(e){
-  const key = parseInt(e.detail || e.which);
-
-  if (key === code[index]){
-    index++;
-    if (index === code.length){
-      alert("Hurray!");
-      index = 0;
-    }
-  } else {
-    index = 0;
-  }
-});
+   if (key === code[index]){
+     index++;
+     if (index === code.length){
+       alert("Hurray!");
+       index = 0;
+     }
+   } else {
+     index = 0;
+   }
+ });
 }
